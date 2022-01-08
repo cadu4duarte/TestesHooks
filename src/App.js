8d36c4f92801ffa23e85582a7a9ebddca7a92a4a
Carlos Eduardo <cadu4duarte@gmail.com>
2022-01-08 20:11:32 -0300

    useMemo e useCallback

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState, useCallback } from "react";
 
 function App() {
 
-  const [tarefas, setTarefas] = useState([
-    "Pagar cartão",
-    "Estudar React"
-  ]);
+  const [tarefas, setTarefas] = useState([]);
+  const [input, setInput] = useState("");
 
-  const [input, setInput] = useState([""]);
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem("tarefas");
+    
+    if(tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
 
-  function handleAdd() {
-    setTarefas([...tarefas, input])
-    setInput("");
-  }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  const handleAdd = useCallback(() => {
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }, [input, tarefas]);
+
+  const totalTarefas = useMemo(()=> tarefas.length, [tarefas]);
 
   return (
     <div >
@@ -21,6 +32,8 @@ function App() {
           <li key={tarefa}>{tarefa}</li>
         ))}
       </ul>
+
+          <strong>Você tem {totalTarefas} tarefas!</strong><br/>
 
           <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
 
